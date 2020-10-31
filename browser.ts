9d91e0444eea72a906ce6b4a047f1a93cff6422c
Sergey Aldoukhov <saldoukhov@gmail.com>
2020-10-30 17:57:16 -0700

    end to end attestation and assertion

--- browser.ts
+++ browser.ts
@@ -1,51 +1,44 @@
 import {
     PublicKeyCredentialCreationOptionsJSON,
+    PublicKeyCredentialRequestOptionsJSON,
     PublicKeyCredentialDescriptorJSON,
-    AttestationCredential
-} from '@simplewebauthn/typescript-types';
+    AttestationCredential,
+    AttestationCredentialJSON,
+    AssertionCredential,
+    AssertionCredentialJSON
+} from '@simplewebauthn/typescript-types'
 
 function base64URLStringToBuffer(base64URLString: string): ArrayBuffer {
-    // Convert from Base64URL to Base64
-    const base64 = base64URLString.replace(/-/g, '+').replace(/_/g, '/');
-    /**
-     * Pad with '=' until it's a multiple of four
-     * (4 - (85 % 4 = 1) = 3) % 4 = 3 padding
-     * (4 - (86 % 4 = 2) = 2) % 4 = 2 padding
-     * (4 - (87 % 4 = 3) = 1) % 4 = 1 padding
-     * (4 - (88 % 4 = 0) = 4) % 4 = 0 padding
-     */
-    const padLength = (4 - (base64.length % 4)) % 4;
-    const padded = base64.padEnd(base64.length + padLength, '=');
+    const base64 = base64URLString.replace(/-/g, '+').replace(/_/g, '/')
+    return Uint8Array.from(atob(base64), c => c.charCodeAt(0))
+}
 
-    // Convert to a binary string
-    const binary = atob(padded);
+function bufferToBase64URLString(buffer: ArrayBuffer): string {
+    const bytes = new Uint8Array(buffer)
 
-    // Convert binary string to buffer
-    const buffer = new ArrayBuffer(binary.length);
-    const bytes = new Uint8Array(buffer);
-
-    for (let i = 0; i < binary.length; i++) {
-        bytes[i] = binary.charCodeAt(i);
-    }
-
-    return buffer;
+    // @ts-ignore
+    const base64String = btoa(String.fromCharCode(...bytes))
+    return base64String
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=/g, '')
 }
 
 function toUint8Array(value: string): Uint8Array {
-    const utf8Encoder = new TextEncoder();
-    return utf8Encoder.encode(value);
+    const utf8Encoder = new TextEncoder()
+    return utf8Encoder.encode(value)
 }
 
 function toPublicKeyCredentialDescriptor(descriptor: PublicKeyCredentialDescriptorJSON): PublicKeyCredentialDescriptor {
-    const {id} = descriptor;
+    const {id} = descriptor
     return {
         ...descriptor,
         id: base64URLStringToBuffer(id),
-    };
+    }
 }
 
-async function startAttestation(creationOptions: string): Promise<string> {
-    const creationOptionsJSON: PublicKeyCredentialCreationOptionsJSON = JSON.parse(creationOptions)
+async function startAttestation(attestationOptions: string): Promise<string> {
+    const creationOptionsJSON: PublicKeyCredentialCreationOptionsJSON = JSON.parse(attestationOptions)
 
     const publicKey: PublicKeyCredentialCreationOptions = {
         ...creationOptionsJSON,
@@ -55,12 +48,65 @@ async function startAttestation(creationOptions: string): Promise<string> {
             id: toUint8Array(creationOptionsJSON.user.id),
         },
         excludeCredentials: creationOptionsJSON.excludeCredentials.map(toPublicKeyCredentialDescriptor),
-    };
+    }
 
-    // console.log(navigator.credentials ? 'yes' : 'no')
+    const credential = (await navigator.credentials.create({publicKey})) as AttestationCredential
+    if (!credential) {
+        throw new Error('Attestation was not completed')
+    }
 
-    const credential = (await navigator.credentials.create({publicKey})) as AttestationCredential;
-    console.log(credential)
+    const {id, rawId, response, type} = credential
 
-    return "Ok"
+    // Convert values to base64 to make it easier to send back to the server
+    const credentialJSON: AttestationCredentialJSON = {
+        id,
+        rawId: bufferToBase64URLString(rawId),
+        response: {
+            attestationObject: bufferToBase64URLString(response.attestationObject),
+            clientDataJSON: bufferToBase64URLString(response.clientDataJSON),
+        },
+        type
+    }
+
+    if (typeof response.getTransports === 'function') {
+        credentialJSON.transports = response.getTransports()
+    }
+
+    return JSON.stringify(credentialJSON)
+}
+
+async function startAssertion(assertionOptions: string): Promise<string> {
+    const assertionOptionsJSON: PublicKeyCredentialRequestOptionsJSON = JSON.parse(assertionOptions)
+
+    const publicKey: PublicKeyCredentialRequestOptions = {
+        ...assertionOptionsJSON,
+        challenge: base64URLStringToBuffer(assertionOptionsJSON.challenge),
+        allowCredentials: assertionOptionsJSON.allowCredentials.map(toPublicKeyCredentialDescriptor)
+    }
+
+    const credential = (await navigator.credentials.get({publicKey})) as AssertionCredential
+    if (!credential) {
+        throw new Error('Assertion was not completed')
+    }
+
+    const {id, rawId, response, type} = credential
+
+    let userHandle = undefined
+    if (response.userHandle) {
+        userHandle = bufferToBase64URLString(response.userHandle)
+    }
+
+    const assertionJSON: AssertionCredentialJSON = {
+        id,
+        rawId: bufferToBase64URLString(rawId),
+        response: {
+            authenticatorData: bufferToBase64URLString(response.authenticatorData),
+            clientDataJSON: bufferToBase64URLString(response.clientDataJSON),
+            signature: bufferToBase64URLString(response.signature),
+            userHandle
+        },
+        type
+    }
+
+    return JSON.stringify(assertionJSON)
 }
--- test.ts
+++ test.ts
@@ -1,45 +1,29 @@
 import {
-    generateAttestationOptions,
+    generateAssertionOptions,
+    generateAttestationOptions, verifyAssertionResponse,
     verifyAttestationResponse,
 } from '@simplewebauthn/server';
-import {PublicKeyCredentialDescriptorJSON} from '@simplewebauthn/typescript-types';
-import {launch, connect} from 'puppeteer'
+import {
+    AttestationCredentialJSON,
+    AssertionCredentialJSON,
+    PublicKeyCredentialDescriptorJSON
+} from '@simplewebauthn/typescript-types';
+import {connect} from 'puppeteer'
 import {transpile} from 'typescript'
 import * as fs from 'fs'
-import { exec, spawn, spawnSync } from 'child_process'
-
-type UserModel = {
-    id: string;
-    username: string;
-    currentChallenge?: string;
-};
-
-// It is strongly advised that authenticators get their own DB
-// table, ideally with a foreign key to a specific UserModel
-type Authenticator = {
-    credentialID: string;
-    publicKey: string;
-    counter: number;
-    // ['usb' | 'ble' | 'nfc' | 'internal']
-    transports?: AuthenticatorTransport[];
-};
+import {spawn} from 'child_process'
 
 // Human-readable title for your website
 const rpName = 'WebAuthn Test';
 // A unique identifier for your website
 const rpID = 'localhost';
 // The URL at which attestations and assertions should occur
-const origin = `https://${rpID}`;
-
-const user: UserModel = {
-    id: 'bob_id',
-    username: 'bob',
-}
+const origin = `http://${rpID}`;
 
 const excludedDevices: PublicKeyCredentialDescriptorJSON[] = []
 
 async function executeApp(command: string, args: string[]): Promise<string> {
-    return new Promise(((resolve, reject) => {
+    return new Promise(((resolve) => {
         const chromeOutput = spawn(command, args)
         chromeOutput.stderr.on('data', (data: Buffer) => {
             resolve(data.toString())
@@ -47,9 +31,9 @@ async function executeApp(command: string, args: string[]): Promise<string> {
     }))
 }
 
-
 const test = async () => {
 
+    // Prepare browser
     const chromeCommand = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
     const chromeArgs = [
         '--remote-debugging-port=9222',
@@ -57,41 +41,99 @@ const test = async () => {
         '--no-default-browser-check',
         '--user-data-dir=./data_dir'
     ]
-
     const chromeOutput = await executeApp(chromeCommand, chromeArgs)
     const chromeUrl = chromeOutput.match(/ws.*/)[0]
+    const browser = await connect({
+        browserWSEndpoint: chromeUrl
+    })
+    const pages = await browser.pages()
+    const page = pages[0]
+    await page.goto('http://localhost')
+    await page.evaluate(() => {
+        document.body.innerHTML = '<br><br><br><H1>ATTESTATION</H1>'
+    })
+    page.on('console', msg => {
+        const text = msg.text()
+        if (!text.endsWith('404 (Not Found)')) {
+            console.log(msg.text());
+        }
+    });
+    await page.addScriptTag({content: transpile(fs.readFileSync('./browser.ts').toString())})
 
-    const options = generateAttestationOptions({
+
+    // WebAuthn attestation
+    console.log("PERFORMING ATTESTATION")
+    const attestationOptions = generateAttestationOptions({
         rpName,
         rpID,
-        userID: user.id,
-        userName: user.username,
+        userID: '12345',
+        userName: 'bob',
         // Don't prompt users for additional information about the authenticator
         // (Recommended for smoother UX)
         attestationType: 'indirect',
         // Prevent users from re-registering existing authenticators
         excludeCredentials: excludedDevices,
     });
+    console.log('\nattestation options:')
+    console.log(attestationOptions)
 
-    const browser = await connect({
-        browserWSEndpoint: chromeUrl
-    })
-
-    const pages = await browser.pages()
-    const page = pages[0]
-    await page.goto('http://localhost')
-
-    page.on('console', msg => {
-        console.log(msg['_text']);
-    });
-
-    await page.addScriptTag({content: transpile(fs.readFileSync('./browser.ts').toString())})
-
-    const token = await page.evaluate((input) => {
+    const attestationResponse = await page.evaluate((input) => {
         // @ts-ignore
         return window.startAttestation(input)
-    }, JSON.stringify(options))
-    console.log(token)
+    }, JSON.stringify(attestationOptions))
+    const attestationCredentialJSON: AttestationCredentialJSON = JSON.parse(attestationResponse)
+    console.log('\nattestation response:')
+    console.log(attestationCredentialJSON)
+
+    const attestationVerification = await verifyAttestationResponse({
+        credential: attestationCredentialJSON,
+        expectedChallenge: attestationOptions.challenge,
+        expectedOrigin: origin,
+        expectedRPID: rpID
+    })
+    console.log('\nattestation verification result');
+    console.log(attestationVerification)
+
+    // WebAuthn assertion
+    console.log("PERFORMING ASSERTION")
+    await page.evaluate(() => {
+        document.body.innerHTML = '<br><br><br><H1>ASSERTION</H1>'
+    })
+
+    const assertionOptions = generateAssertionOptions({
+        // Require users to use a previously-registered authenticator
+        allowCredentials: [{
+            id: attestationVerification.authenticatorInfo.base64CredentialID,
+            type: 'public-key',
+            // Optional
+            transports: ['usb'],
+        }],
+        userVerification: 'preferred',
+    });
+    console.log(assertionOptions)
+
+    const assertionResponse = await page.evaluate((input) => {
+        // @ts-ignore
+        return window.startAssertion(input)
+    }, JSON.stringify(assertionOptions))
+    const assertionCredentialJSON: AssertionCredentialJSON = JSON.parse(assertionResponse)
+    console.log('\nassertion response:')
+    console.log(assertionCredentialJSON)
+
+    const assertionVerification = await verifyAssertionResponse({
+        credential: assertionCredentialJSON,
+        expectedChallenge: assertionOptions.challenge,
+        expectedOrigin: origin,
+        expectedRPID: rpID,
+        authenticator: {
+            counter: attestationVerification.authenticatorInfo.counter,
+            credentialID: attestationVerification.authenticatorInfo.base64CredentialID,
+            publicKey: attestationVerification.authenticatorInfo.base64PublicKey
+        }
+    })
+    console.log('\nassertion verification result');
+    console.log(assertionVerification)
+
     await browser.close()
 };
 
